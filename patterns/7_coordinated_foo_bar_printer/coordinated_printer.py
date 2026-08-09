57514717282535b6180a5f865ba9af2fa5d1a0de
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-08-09 22:15:40 +0530

    add similarity of the coordinated printer problem to the producer consumer problem with one slot

--- patterns/7_coordinated_foo_bar_printer/coordinated_printer.py
+++ patterns/7_coordinated_foo_bar_printer/coordinated_printer.py
@@ -3,6 +3,14 @@ from threading import Semaphore
 # Very similar to problem 6 but with 2 systems
 # so we could use semaphores to solve this problem (along with
 # the condition variable strategies used in problem 6)
+
+# Think of this problem as the bounded buffer problem (producer-consumer)
+# with just one slot in the buffer where
+# a producer putting an item in the buffer as printing "Foo"
+# a consumer consuming an item from the buffer as printing "Bar"
+# Now it is the OSTEP textbook solution of the producer consumer problem
+# with one buffer slot using semaphores and we don't need any locks here
+# as there isn't any state we are updating which means no critical sections
 class CoordinatedPrinter:
     def __init__(self, n):
         self.n = n
