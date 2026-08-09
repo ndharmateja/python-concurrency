be420bd9c7406a15fcd59408846def43db5291da
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-08-09 21:40:54 +0530

    add driver code for the ordered log processor

---
+++ patterns/6a_ordered_log_processor/driver.py
@@ -0,0 +1,44 @@
+from threading import Thread
+import time
+import random
+from ordered_log_processor_2 import OrderedLogProcessor
+
+def worker_a(processor: OrderedLogProcessor, iterations):
+    for _ in range(iterations):
+        time.sleep(random.uniform(0.1, 0.5))
+        print("log line from A generated")
+        processor.printFirst("[A] log line")
+
+def worker_b(processor: OrderedLogProcessor, iterations):
+    time.sleep(2)
+    for _ in range(iterations):
+        time.sleep(random.uniform(0.5, 1))
+        print("log line from B generated")
+        processor.printSecond("[B] log line")
+
+def worker_c(processor: OrderedLogProcessor, iterations):
+    for _ in range(iterations):
+        time.sleep(random.uniform(1, 3))
+        print("log line from C generated")
+        processor.printThird("[C] log line")
+
+
+def main():
+    processor = OrderedLogProcessor()
+    rounds = 4 
+
+    targets = [worker_c, worker_b, worker_a]
+    threads = []
+
+    for target in targets:
+        thread = Thread(target=target, args=(processor, rounds))
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    print("All rounds finished!")
+
+if __name__ == "__main__":
+    main()
