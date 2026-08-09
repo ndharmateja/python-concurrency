e83af0e0ba682c2c8edff311022abfa3d3a71947
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-08-09 21:54:53 +0530

    solve the foo bar coordinated printer problem

---
+++ patterns/6b_coordinated_foo_bar_printer/coordinated_printer.py
@@ -0,0 +1,21 @@
+from threading import Semaphore
+
+# Very similar to 6a but with 2 systems
+# so we could use semaphores to solve this problem
+class CoordinatedPrinter:
+    def __init__(self, n):
+        self.n = n
+        self.foo_sem = Semaphore(1)
+        self.bar_sem = Semaphore(0)
+
+    def printFoo(self):
+        for _ in range(self.n):
+            self.foo_sem.acquire()
+            print("Foo")
+            self.bar_sem.release()
+
+    def printBar(self):
+        for _ in range(self.n):
+            self.bar_sem.acquire()
+            print("Bar")
+            self.foo_sem.release()
---
+++ patterns/6b_coordinated_foo_bar_printer/driver.py
@@ -0,0 +1,27 @@
+from threading import Thread
+from coordinated_printer import CoordinatedPrinter
+from time import sleep
+
+# Very similar to 6a but with 2 systems
+
+def foo_worker(processor: CoordinatedPrinter):
+    processor.printFoo()
+
+def bar_worker(processor: CoordinatedPrinter):
+    sleep(2)
+    processor.printBar()
+
+def main():
+    processor = CoordinatedPrinter(10)
+
+    foo_thread = Thread(target=foo_worker, args=(processor,))
+    bar_thread = Thread(target=bar_worker, args=(processor,))
+    foo_thread.start()
+    bar_thread.start()
+    foo_thread.join()
+    bar_thread.join()
+
+    print("All rounds finished!")
+
+if __name__ == "__main__":
+    main()
